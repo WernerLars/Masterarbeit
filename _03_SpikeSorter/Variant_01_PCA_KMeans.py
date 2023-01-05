Used Contigency matrix to fix bugs with mappings implemented computation of centroids for autoencoder/qlearning variants

_03_SpikeSorter/Variant_01_PCA_KMeans.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from _01_LoadDataset.LoadingDataset import LoadDataset
-from _04_Visualisation.Visualisation import visualisingClusters, printConfusionMatrix
+from _04_Visualisation.Visualisation import visualisingClusters, printConfusionMatrix, getClusterCenters
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
@@ -31,8 +31,8 @@ def Variant_01_PCA_KMeans(path):
         x.append(spike[0])
         y.append(spike[1])
 
-    print(kmeans.cluster_centers_)
+    centroids = getClusterCenters(pca_transformed, kmeans.labels_)
+    print("Centroids: ", type(centroids))
 
-    visualisingClusters(x, y, kmeans.labels_, kmeans.cluster_centers_)
-
+    visualisingClusters(x, y, kmeans.labels_, centroids)
     printConfusionMatrix(y_labels, kmeans.labels_, np.unique(y_labels))

_03_SpikeSorter/Variant_04_Autoencoder_QLearning.py
@@ -119,6 +119,8 @@ def test(dataloader, y_test, model, ql):
     print(y_l)
     print(ql.clusters)
 
-    # Last 100 Spikes are visualised in a Graph and evaluated in Confusion Matrix
-    visualisingClusters(encoded_features_X[-100:], encoded_features_Y[-100:], ql.clusters[-100:])
-    printConfusionMatrix(y_l[-100:], ql.clusters[-100:], np.unique(ql.clusters[-100:]))
+    # Visualisation only with the last 100 Spikes
+    centroids = getClusterCenters(encoded_features_list[-100:], ql.clusters[-100:])
+    visualisingClusters(encoded_features_X[-100:], encoded_features_Y[-100:], ql.clusters[-100:], centroids)
+
+    printConfusionMatrix(y_l, ql.clusters, np.unique(y_l))

_04_Visualisation/Visualisation.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import sort
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
+from sklearn.metrics.cluster import contingency_matrix
 
 
 def visualisingClusters(x, y, cluster, centroids=None):
@@ -21,6 +23,22 @@ def visualisingClusters(x, y, cluster, centroids=None):
     plt.legend(loc="upper left")
     plt.show()
     return
+
+
+def getClusterCenters(features_list, labels):
+    centroids = []
+    unique_cluster = sort(np.unique(labels))
+    for cluster in unique_cluster:
+        x_list = []
+        y_list = []
+        for index in range(0, len(labels)):
+            if cluster == labels[index]:
+                x_list.append(features_list[index][0])
+                y_list.append(features_list[index][1])
+        x_mean = np.mean(x_list)
+        y_mean = np.mean(y_list)
+        centroids.append([x_mean, y_mean])
+    return np.asarray(centroids)
 
 
 def printSpike(spike, n_features, color):
@@ -44,18 +62,18 @@ def visualisingReconstructedSpike(original, reconstructed, n_features, cluster):
 
 
 def printConfusionMatrix(ground_truth, predictions, labels):
-    cm = confusion_matrix(ground_truth, predictions, labels=labels)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
-    disp.plot()
-    plt.title("Without Cluster Mapping")
-    plt.show()
+    cm = contingency_matrix(ground_truth, predictions)
+    print("Contingency Matrix: ")
+    print(cm)
 
     mapping = []
-    for true in labels:
+    for predicted in range(0, len(np.unique(predictions))):
         get_truth_values = []
-        for predicted in labels:
-            get_truth_values.append(cm[predicted][true])
+        for true in labels:
+            get_truth_values.append(cm[true][predicted])
         mapping.append(np.argmax(get_truth_values))
+
+    print("Mapping: ", mapping)
 
     predictions_mapping = []
     for prediction in predictions: