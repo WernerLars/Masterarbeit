5edbbdbf21b973f9fd13dd20b534c3a370fb007a
Lars <www.lotty1308@gmx.de>
2023-04-09 23:34:34 +0200

    Update of Base Line with Punishment Coefficient Experiment

--- _05_Experiments/Base_Line_W_PC.py
+++ _05_Experiments/Base_Line_W_PC.py
@@ -10,32 +10,42 @@ from _04_Visualisation import Tables
 
 
 def main():
-    pc = 0.7
-    main_path = f"Base_Line_W_PC_{pc}/"
+    main_path = f"Base_Line_W_PC/"
 
     if os.path.exists(main_path) is False:
         os.mkdir(main_path)
-    else:
-        return
 
-    p1 = Process(target=Experiment_01.main, args=(main_path,))
-    p1.start()
-    p2 = Process(target=Experiment_02.main, args=(main_path,))
-    p2.start()
-    p3 = Process(target=Experiment_03.main, args=(main_path, 0, pc))
-    p3.start()
-    p4 = Process(target=Experiment_04.main, args=(main_path, 0, pc))
-    p4.start()
-    p5 = Process(target=Experiment_05.main, args=(main_path, 0, pc))
-    p5.start()
+    punishment_coefficients = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
+    variant_paths = []
+    jobs = []
 
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
+    for pc in punishment_coefficients:
+        variant_path = f"{main_path}/{pc}/"
+        variant_paths.append(variant_path)
+        if os.path.exists(variant_path) is False:
+            os.mkdir(variant_path)
 
-    Tables.main(experiment_path=main_path)
+        p1 = Process(target=Experiment_01.main, args=(variant_path,))
+        p1.start()
+        jobs.append(p1)
+        p2 = Process(target=Experiment_02.main, args=(variant_path,))
+        p2.start()
+        jobs.append(p2)
+        p3 = Process(target=Experiment_03.main, args=(variant_path, 0, pc))
+        p3.start()
+        jobs.append(p3)
+        p4 = Process(target=Experiment_04.main, args=(variant_path, 0, pc))
+        p4.start()
+        jobs.append(p4)
+        p5 = Process(target=Experiment_05.main, args=(variant_path, 0, pc))
+        p5.start()
+        jobs.append(p5)
+
+        for job in jobs:
+            job.join()
+
+    for variant_path in variant_paths:
+        Tables.main(experiment_path=variant_path)
 
 
 if __name__ == '__main__':
